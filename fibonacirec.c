b69b3a16170cf9b8a6a907e96fb4ffe5f3a5e495
SourabhPatil <202251095@iiitvadodara.ac.in>
2023-02-26 11:26:04 +0530

    commit

--- fibonacirec.c
+++ fibonacirec.c
@@ -1,20 +1,21 @@
-//Program to print fibonaci series till n
-#include<stdio.h>
-int fib(int);
-int main()
+//Program to write fibonacci series till n terms
+#include <stdio.h>
+int fib(int,int,int);
+int main() 
 {
     int n;
-    scanf("%d",&n);
-    fib(n);
+    printf("Write the number till which you want fibonacci series:-");
+    scanf("%d", &n);
+    printf("Fibonacci series till %d terms:",n);
+    fib(n, 0, 1);
+    return 0;
 }
-int fib(int n)
+int fib(int n, int a, int b) 
 {
-    if (n<=1)
-    {
-        return n;
+    if (n == 0) {
+        return 0;
     }
-    else
-    {
-        return(fib(n-1)+fib(n-2));
-    }
+    printf("%d ", a);
+    return fib(n - 1, b, a + b);
 }
+
--- test.c
+++ test.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-void reverseString(char str[], int s, int n) 
-{
-    if (s>= n) {
+
+// Recursive function to reverse a string
+void reverseString(char str[], int start, int end) {
+    if (start >= end) {
         return;
     }
-    char temp = str[s];
-    str[s] = str[n];
-    str[n] = temp;
-    reverseString(str, s + 1, n - 1);
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseString(str, start + 1, end - 1);
 }
 
 int main() {
     char str[100];
-    int n;
     printf("Enter a string: ");
     fgets(str, 100, stdin);
     str[strcspn(str, "\n")] = '\0'; // remove newline character from input
 
-    reverseString(str, 0, n - 1);
+    reverseString(str, 0, strlen(str) - 1);
 
     printf("Reversed string: %s\n", str);
 
